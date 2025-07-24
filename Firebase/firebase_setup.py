217242c8b8cd935099515c10338ca99c230bf3e6
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2025-07-24 21:07:35 +0530

    firebase linking with project and twilio

--- Firebase/firebase_setup.py
+++ Firebase/firebase_setup.py
@@ -1,6 +1,29 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials,storage, firestore
+import datetime
+from io import BytesIO
 
-cred = credentials.Certificate("Firebase/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("Firebase/sahayak-mitra-firebase-adminsdk-fbsvc-caa1fa3039.json")
+firebase_admin.initialize_app(cred,{"storageBucket": "sahayak-mitra"})
 
+
+
+bucket=storage.bucket()
+
+def upload_file_to_firebase(sender, content_type,file,file_extension,userWaId=None):
+    file_name= f"{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.{file_extension}"
+    print(cred.project_id)
+    destination_blob_name = "Chat_files/"
+    if sender == "user":
+        destination_blob_name += "user/"+userWaId+"/"
+    elif sender == "bot":
+        destination_blob_name += "bot/"
+    destination_blob_name += file_name
+    print(f"Uploading file to {destination_blob_name} in Firebase Storage")
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_file(BytesIO(file),content_type=content_type)
+    blob.make_public()
+    print(f"File {destination_blob_name} uploaded to {destination_blob_name}.")
+    return f"https://storage.googleapis.com/sahayak-mitra/{destination_blob_name}"
+    
+    
--- twilio_whatsapp.py
+++ twilio_whatsapp.py
@@ -3,6 +3,9 @@ from flask import Flask, request, Response
 from twilio.twiml.messaging_response import MessagingResponse 
 from dotenv import load_dotenv
 import os
+import requests
+from requests.auth import HTTPBasicAuth
+from Firebase.firebase_setup import upload_file_to_firebase
 
 from router import getResponse
 
@@ -26,6 +29,19 @@ def send_whatsapp_message(to,body):
 
 
 
+# send_media_message('+917569105854', ['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80')]
+def send_media_message(to, media_url):
+    print("Sending media message to:", to)
+    print("Media URL:", media_url)
+    message = client.messages.create(
+        from_=twilio_whatsapp_number,
+        media_url=media_url,
+        to=f'whatsapp:{to}'
+    )
+    return message
+
+
+
 app=Flask(__name__)
 
 @app.route('/')
@@ -37,7 +53,38 @@ def reply():
     print("Received a message")
     userResponse=request.values.get('Body') 
     print("User response:", userResponse)
-    responseString=getResponse(userResponse)
+    print("Num media:- ", request.values.get('NumMedia'))
+    if request.values.get('NumMedia') and int(request.values.get('NumMedia')) > 0:
+        print("Received media message")
+        media_urls = [request.values.get(f'MediaUrl{i}') for i in range(int(request.values.get('NumMedia')))]
+        for i in range(int(request.values.get('NumMedia'))):
+            print("Media URL:", request.values.get('MediaUrl'+str(i)))
+            content_type= request.values.get('MediaContentType'+str(i), '')
+            file_extension = content_type.split('/')[-1]
+            media_url = request.values.get('MediaUrl'+str(i))
+            response = requests.get(media_url, auth=HTTPBasicAuth(account_sid, auth_token))
+            print("content_type:", content_type)
+            print("file_extension:", file_extension)
+            print("Media URL:", media_url)
+            
+            if response.status_code == 200:
+                firebase_file_link=upload_file_to_firebase("user",content_type, response.content, file_extension, userWaId=request.values.get('From').split(':')[1])
+                
+                print("File uploaded successfully")
+                send_media_message(request.values.get('From').split(':')[1], [firebase_file_link])
+                print("message sent successfully")
+                return Response(status=200)
+                
+            else:
+                print("Response failed. Status code:", response.status_code)
+            
+            
+            
+        print("Media URLs:", media_urls)
+        responseString = "Received media: " + ", ".join(media_urls)
+        
+    else:
+        responseString='getResponse(userResponse)'
     
     print(request.values)
     userNumber=request.values.get('From')
@@ -45,12 +92,17 @@ def reply():
     print(userNumber)    
     response= MessagingResponse()
     response.message(responseString)
+    response.message("This is a test message from Sahayak Mitra.")
     return str(response)
+
+
     
     
 if __name__ == '__main__':
     # send_whatsapp_message('+917569105854', "Hello! This is a test message from Sahayak Mitra.")
     # port = int(os.environ.get("PORT", 8080))  # Cloud Run uses PORT env variable
     # app.run(host="0.0.0.0", port=port)
+    
+    
     
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))  # Run locally on port 5000 for testing
